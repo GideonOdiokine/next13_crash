13ebc3222b226dd473928f00dbedb473a5811f4e
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2022-11-01 04:44:56 +0100

    page layout

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Gidysmart Media",
+  description: "Web development",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        <main className="container">{children}</main>
+      </body>
+    </html>
+  );
+}
