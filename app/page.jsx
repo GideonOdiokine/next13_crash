cecf10343673edabaa05f98bd2011766e38a0d37
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2022-11-07 05:53:53 +0100

    updated homepage

--- app/page.jsx
+++ app/page.jsx
@@ -5,17 +5,6 @@ const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
-      <ul>
-        <li>
-          <Link href="/">Home </Link>
-        </li>
-        <li>
-          <Link href="/about">About </Link>
-        </li>
-        <li>
-        <Link href="/about/team">Team </Link>
-        </li>
-      </ul>
     </div>
   );
 }
