60eedf7a75bf84ba6276c7606760b5214170b8a5
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2022-11-10 06:31:12 +0100

    import Course component to display courses

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
+import Courses from './components/Courses';
 
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <Courses />
     </div>
   );
 }
