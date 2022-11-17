dde2b11b32cebab97a7277a1f1c89563336444aa
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2022-11-17 07:02:28 +0100

    SearchInput added to homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import Link from 'next/link'
-import Courses from './components/Courses';
+"use client";
+import React, { useState, useEffect } from "react";
+// import Link from "next/link";
+import Courses from "./components/Courses";
+import LoadingPage from "./loading";
+import SearchInput from "./components/SearchInput";
 
 const HomePage = () => {
+  const [loading, setLoading] = useState(true);
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch("/api/courses");
+      const data = await res.json();
+      setCourses(data);
+      setLoading(false);
+    };
+    fetchCourses();
+  }, []);
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
   return (
     <div>
-      <h1>Home Page</h1>
-      <Courses />
+      <SearchInput getSearchResults={(result) => setCourses(result)} />
+      <Courses courses={courses} />
     </div>
   );
-}
+};
 
-export default HomePage
+export default HomePage;
