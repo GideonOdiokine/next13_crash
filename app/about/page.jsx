75945eb89c110f6b5af43de3e000152a2c6caec3
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2023-07-15 04:24:18 +0100

    page routing

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+        <h1> About Gideon Media</h1>
+        <p> We are a media company that specializes in creating content for the web. </p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/about/team/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TeamPage = () => {
+  return (
+    <div>
+        <h1> Our Team </h1>
+        <p> We are a small team of developers and designers. </p>
+    </div>
+  )
+}
+
+export default TeamPage
